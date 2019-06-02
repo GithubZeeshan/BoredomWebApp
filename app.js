51e063fe50d8d4a06161086168a817a8833a75c3
Zeeshan Khan <zeeshankhandeveloper@gmail.com>
2019-06-03 03:43:46 +0800

    Initial setup

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+var config = require("./public/config.js");
+
+//tell express to serve "public" directory on server
+app.use(express.static("public"));
+
+//tell express to use ejs format as default
+app.set("view engine", "ejs");
+
+//index route
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+//serve app on port 3000
+app.listen(3000, () => console.log('Boredom Web App being served on Port 3000'));
