6fa0b1e8d37e04b7f2ee6626fd6ecb4fa3af9143
Zeeshan Khan <zeeshankhandeveloper@gmail.com>
2019-06-03 03:45:04 +0800

    fix config error in app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var app = express();
 var request = require("request");
-var config = require("./public/config.js");
 
 //tell express to serve "public" directory on server
 app.use(express.static("public"));
