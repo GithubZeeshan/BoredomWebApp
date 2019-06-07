985a2802d7bde4fa499c485d1fc62ac8ba309c00
Zeeshan Khan <zeeshankhandeveloper@gmail.com>
2019-06-08 00:01:24 +0800

    Add index, result routes

--- app.js
+++ app.js
@@ -13,5 +13,27 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
+app.get("/result", (req, res) => {
+
+    if (req.query.activityType == undefined && req.query.numParticipants == undefined){
+        var url = 'http://www.boredapi.com/api/activity';
+    }
+    else{
+        var url = 'http://www.boredapi.com/api/activity?type=' + req.query.activityType;
+    }
+
+    request(url, (error, response, body) => {
+        if (error){
+            console.log("Error");
+            res.render("home");
+        }
+        else{
+            var data = JSON.parse(body);
+            console.log(data);
+            res.render("result", {data: data});
+        }
+    });
+});
+
 //serve app on port 3000
 app.listen(3000, () => console.log('Boredom Web App being served on Port 3000'));
